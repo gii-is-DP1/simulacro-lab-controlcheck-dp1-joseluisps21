3a74438684e58da3963fb593e91e5b2574fb373f
joseluisps21 <joseluissfc4@gmail.com>
2022-01-13 21:30:46 +0100

    simulacro

--- src/main/java/org/springframework/samples/petclinic/product/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -2,21 +2,38 @@ package org.springframework.samples.petclinic.product;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+
     public List<Product> getAllProducts(){
-        return null;
+        List<Product> p = productRepository.findAll();
+        return p;
     }
 
     public List<Product> getProductsCheaperThan(double price) {
-        return null;
+        List<Product> p = this.productRepository.findByPriceLessThan(price);
+        return p;
     }
 
     public ProductType getProductType(String typeName) {
-        return null;
+        ProductType tipoProducto = productRepository.findProductType(typeName);
+        return tipoProducto;
+    }
+
+    public List<ProductType> findAllProductTypes(){
+        List<ProductType> pt = productRepository.findAllProductTypes();
+        return pt;
     }
 
     public Product save(Product p){
-        return null;       
+        return productRepository.save(p);     
     }
 
     
